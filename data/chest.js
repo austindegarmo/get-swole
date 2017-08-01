62811cf103335fffa638764f38607fae56a5c545
aparvin2k <aparvin2005@gmail.com>
2017-08-01 14:08:14 -0700

    added js files for the youtube tutorials for modals

---
+++ data/chest.js
@@ -0,0 +1,22 @@
+//create array of sample workouts
+
+var chest = [{
+  name: "chest",
+  trailer: "https://www.youtube.com/watch?v=gRVjAtPip0Y"
+}, {
+  name: "chest",
+  trailer: "https://www.youtube.com/watch?v=ZOwwBk642SI"
+},{
+  name: "chest",
+  trailer: "https://www.youtube.com/watch?v=dtNXLavPFo0"
+}, {
+  name: "chest",
+  trailer: "https://www.youtube.com/watch?v=07Bcqtib4FM"
+}, {
+  name: "chest",
+  trailer: "https://www.youtube.com/watch?v=eozdVDA78K0"
+}];
+
+ //export array
+
+module.exports = chest;
---
+++ data/legs.js
@@ -0,0 +1,18 @@
+var legs = [{
+  name: "legs",
+  trailer: "https://www.youtube.com/watch?v=Dy28eq2PjcM"
+}, {
+  name: "legs",
+  trailer: "https://www.youtube.com/watch?v=1Tq3QdYUuHs"
+},{
+  name: "legs",
+  trailer: "https://www.youtube.com/watch?v=-M4-G8p8fmc"
+}, {
+  name: "legs",
+  trailer: "https://www.youtube.com/watch?v=DGavj41F_Cs"
+}, {
+  name: "legs",
+  trailer: "https://www.youtube.com/watch?v=hxldG9FX4j4"
+}];
+
+module.exports = legs;
