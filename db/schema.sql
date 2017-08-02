642db37f43104342a396305eb37c0645a9baea00
aparvin2k <aparvin2005@gmail.com>
2017-08-02 14:27:42 -0700

    updated my files

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE exercise_samples (
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  exercise_type VARCHAR(100) NOT NULL,
+  youTube_trailer VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ models/chest.js
@@ -0,0 +1,39 @@
+module.exports = function(sequelize, DataTypes) {
+  var chestExercise = sequelize.define("chest_exercise", {
+    title: {
+      exercise_name: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    weight_1: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    set_1: {
+      type: DataTypes.INTEGER,
+      isNumber: true,
+      isInt: true
+    }, 
+    weight_2: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    set_2: {
+      type: DataTypes.INTEGER,
+      isNumber: true,
+      isInt: true
+    },
+    weight_3: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    set_3:{
+      type: DataTypes.INTEGER,
+      isNumber: true,
+      isInt: true
+    }
+  });
+  return chestExercise;
+};
