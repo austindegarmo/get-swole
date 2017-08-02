711741803e687a4b400c6cc2656963a82aac03d8
Austin DeGarmo <austindegarmo@yahoo.com>
2017-08-02 11:51:18 -0700

    bullshit

---
+++ routes/api-routes.js
@@ -0,0 +1,38 @@
+var db = require("..models");
+
+module.exports = function(app) {
+
+	app.get(".my-info-1", function(req, res) {
+		db."tablename".findAll({}).then(function(dbUser) {
+
+			res.json(dbdatabasename);
+		});
+	});
+
+app.post("/api/workoutname", function(req, res) {
+
+	db."databasename".create({
+
+		text: req.body."tabledata"
+		type: req.body."tabledata"
+	}).then(function(db"dbname") {
+
+		res.json(db"dbname");
+	});
+
+	
+});
+
+app.put("/api/workoutname", function(req, res) {
+
+
+})
+
+
+
+
+
+
+
+
+}
