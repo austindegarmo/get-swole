9c27888d076e1bef3bbf25df03f09fc51d2c3635
jennie snider <jenniesnider@calvisitor-10-105-141-34.calvisitor.1918.berkeley.edu>
2017-08-03 15:37:51 -0700

    adding chart.js files

---
+++ public/assets/js/app.js
@@ -0,0 +1,76 @@
+
+
+$(document).ready(function(){
+var Chart = require('chart.js');
+
+var chartData = {
+    data: {
+        labels:[1,2,3,4,5],
+        datasets: [{
+            label: 'progress',
+            data: [5,7,12,20,25],
+            backgroundColor: [
+                'rgba(200, 200, 200, 1)'
+            ],
+            borderColor:[
+                'rgba(200, 200, 200, 1)'
+            ]
+
+        }],
+    }
+};
+var ctx = document.getElementById("chart");
+
+var progChart = new Chart(ctx, {
+    type: 'area',
+    data: chartData
+})
+
+});
+
+
+//get data for each exercise
+
+
+//return number to represent specific exercise
+
+var exArray = [];
+
+function getTotalLifted(){
+    var exTotal = 0
+    for (var i = 0; i < exDataArray.length; i++) {
+        var TL = (exArray[i].weight1 * exArray.set1) + (exArray[i].weight2 * exArray.set2) + (exArray[i].weight3 * exArray.set3);
+        TL.push(exArray);
+    }
+}
+
+// return number to represent all exercises for a muscle group
+
+var catArray = []; //meow
+function getCatTotal(){
+    var catTotal = 0;
+    for (var i = 0; i < catTotalArray.length; i++) {
+        catTotal += catTotalArray[i]
+        catTotal.push(catArray);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
